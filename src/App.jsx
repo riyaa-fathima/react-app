669d9ea55f0f26adcb67b365f4492f6745554096
riyafathima2005 <riyafathima9889@gmail.com>
2025-07-24 20:44:50 +0530

    increment decrement button created

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,27 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(2)
 
-//   function increment () {
+  function increment () {
      
-//      setCount(count + 1);
-//      console.log(count)
+     setCount(count + 1);
+     console.log(count)
 
-//     }
+    }
 
-//   function decrement (){
-//     setCount (count-1);
+  function decrement (){
+    setCount (count-1);
 
-//   }  
-//   const handleSubmit = () => {
-//     alert ("button clicked")  }
+  }  
+  
 
 
-//   return (
-//     <>
-//     <form onSubmit={handleSubmit}>
-//       <input type="text" />
-//       <button type='submit'> submit </button>
-//     </form>
-//     <h2>Number {count}</h2>
-//     <button onClick={increment}> increment</button>
-//     <button onClick={decrement}>decrement</button>
-//     </>
-//   )
+  return (
+    <>
+    <h2>Number {count}</h2>
+    <button onClick={increment}> increment</button>
+    <button onClick={decrement}>decrement</button>
+    </>
+  )
  }
 
 export default App
